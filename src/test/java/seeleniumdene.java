31684d5a5ee0a1af38e12bef5fb3bcab0dec9db4
gnctech <genca52@gmail.com>
2023-03-27 00:39:38 +0300

    seleniumv1.0.0

---
+++ src/test/java/seeleniumdene.java
@@ -0,0 +1,16 @@
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class seeleniumdene {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.techproeducation.com");
+
+
+
+
+    }
+}
